81c4246e65b49cff591b69cd1f22572d706267e9
juwon <58173061+juwon0605@users.noreply.github.com>
2021-10-22 00:00:47 +0900

    1. 가장 가까운 두 점

---
+++ 6.코딩 테스트 실전 모의고사/구름에듀/SEASON4/1회/1. 가장 가까운 두 점.cpp
@@ -0,0 +1,37 @@
+﻿/*
+1. 가장 가까운 두 점
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	long long first = 0, second = 9999999999;
+	int n;
+	cin >> n;
+	vector<long long> V(n);
+	for (int i = 0; i < n; i++) {
+		cin >> V[i];
+	}
+	sort(V.begin(), V.end());
+	for (int i = 0; i < n - 1; i++) {
+		if (second - first > V[i + 1] - V[i]) {
+			first = V[i];
+			second = V[i + 1];
+		}
+	}
+	cout << first << " " << second;
+	return 0;
+}
+
+
+/*
+모범 답안
+	시간복잡도
+		O(NlogN)
+	공간복잡도
+		O(N)
+*/
