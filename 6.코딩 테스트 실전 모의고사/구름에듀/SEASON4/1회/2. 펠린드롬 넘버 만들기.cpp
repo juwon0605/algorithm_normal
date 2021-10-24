4a7ad98e0b51ef7adc9a02725a609c4c03ab86be
juwon <58173061+juwon0605@users.noreply.github.com>
2021-10-24 15:56:01 +0900

    2. 펠린드롬 넘버 만들기

---
+++ 6.코딩 테스트 실전 모의고사/구름에듀/SEASON4/1회/2. 펠린드롬 넘버 만들기.cpp
@@ -0,0 +1,54 @@
+﻿/*
+2. 펠린드롬 넘버 만들기
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int F(int);
+bool isValid(int);
+
+int main() {
+	int num, res = -1;
+	cin >> num;
+	for (int i = 0; i < 3; i++) {
+		num = F(num);
+		if (num >= 10000) {
+			break;
+		}
+		if (isValid(num)) {
+			res = num;
+			break;
+		}
+	}
+	cout << res;
+	return 0;
+}
+
+int F(int num) {
+	string str1, str2;
+	str1 = to_string(num);
+	for (int i = str1.size() - 1; i >= 0; i--) {
+		str2 += str1[i];
+	}
+	return stoi(str1) + stoi(str2);
+}
+bool isValid(int num) {
+	string str = to_string(num);
+	for (int i = 0; i < str.size(); i++) {
+		if (str[i] != str[str.size() - 1 - i]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+/*
+모범 답안
+	시간복잡도
+		O(L) (L: length of N)
+	공간복잡도
+		O(L)
+*/
