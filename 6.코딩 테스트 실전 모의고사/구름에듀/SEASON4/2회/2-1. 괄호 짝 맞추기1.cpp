1b90c41a26a222638be287ed8bc869a17cc8618c
juwon <58173061+juwon0605@users.noreply.github.com>
2021-10-28 15:51:05 +0900

    2-1. 괄호 짝 맞추기1

---
+++ 6.코딩 테스트 실전 모의고사/구름에듀/SEASON4/2회/2-1. 괄호 짝 맞추기1.cpp
@@ -0,0 +1,48 @@
+﻿/*
+2-1. 괄호 짝 맞추기1
+*/
+
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+bool check(string);
+
+int main() {
+	string str;
+	int t;
+	cin >> t;
+	while (t--) {
+		cin >> str;
+		cout << (check(str) ? "YES" : "NO") << "\n";
+	}
+	return 0;
+}
+
+bool check(string input) {
+	stack<char> S;
+	string open = "{[(";
+	string close = "}])";
+	for (int i = 0; i < input.size(); i++) {
+		if (input[i] == '{' || input[i] == '[' || input[i] == '(') {
+			S.push(input[i]);
+		}
+		else if (input[i] == '}' || input[i] == ']' || input[i] == ')') {
+			if (S.empty()) return false;
+			for (int j = 0; j < 3; j++) {
+				if (input[i] == close[j] && S.top() != open[j]) return false;
+			}
+			S.pop();
+		}
+	}
+	return S.empty();
+}
+
+/*
+모범 답안
+	시간복잡도
+		O(TN)
+	공간복잡도
+		O(N)
+*/
